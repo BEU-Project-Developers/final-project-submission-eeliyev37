d7a8c849de3320a2fddbbb4f954ce1f057382d82
Ekber <eeliyev37@std.beu.edu.az>
2024-06-19 21:28:46 +0400

    Final Project

---
+++ MkRestaurant/MVCHamburgerciOtomasyonu/Controllers/AuthController.cs
@@ -0,0 +1,136 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MVCHamburgerciOtomasyonu.Entity.Entities;
+using MVCHamburgerciOtomasyonu.Service.DTOs.Users;
+using MVCHamburgerciOtomasyonu.Web.Consts;
+using NToastNotify;
+
+namespace MVCHamburgerciOtomasyonu.Web.Controllers
+{
+	public class AuthController : Controller
+	{
+		private readonly UserManager<AppUser> userManager;
+		private readonly SignInManager<AppUser> signInManager;
+		private readonly IMapper _mapper;
+		private readonly IToastNotification toastNotification;
+		public AuthController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, IMapper mapper, IToastNotification notification)
+		{
+			toastNotification = notification;
+			this.userManager = userManager;
+			this.signInManager = signInManager;
+			_mapper = mapper;
+		}
+
+		[HttpGet]
+		public IActionResult Login()
+		{
+
+			return View();
+		}
+		[AllowAnonymous]
+		[HttpPost]
+		public async Task<IActionResult> Login(UserLoginDto userLoginDto)
+		{
+			if (ModelState.IsValid)
+			{
+				var user = await userManager.FindByEmailAsync(userLoginDto.Email);
+				if (user != null)
+				{
+					var result = await signInManager.PasswordSignInAsync(user, userLoginDto.Password, userLoginDto.RememberMe, false);
+					if (result.Succeeded)
+					{
+						var roles = await userManager.GetRolesAsync(user);
+						if (roles.Contains(RoleConsts.Superadmin))
+						{
+							return RedirectToAction("AdminDashboard", "Dashboard");
+						}
+						if (roles.Contains(RoleConsts.User))
+						{
+
+							return RedirectToAction("UserDashboard", "Dashboard");
+						}
+					}
+					else
+					{
+						ModelState.AddModelError("", "E-mail or password is not correct.");
+						return View();
+					}
+				}
+				else
+				{
+					ModelState.AddModelError("", "E-mail or password is not correct.");
+					return View();
+				}
+			}
+			else
+			{
+				ModelState.AddModelError("", "E-mail or password is not correct.");
+				return View();
+			}
+			return View();
+		}
+		[AllowAnonymous]
+		[HttpGet]
+		public IActionResult Register()
+		{
+
+			return View();
+		}
+		[AllowAnonymous]
+		[HttpPost]
+		public async Task<IActionResult> Register(CreateUserDto createUserDto)
+		{
+			if (!ModelState.IsValid)
+			{
+				return View();
+			}
+
+			if (createUserDto.Password != createUserDto.ConfirmPassword)
+			{
+				ModelState.AddModelError("", "Passwords do not match.");
+				return View();
+			}
+			if (createUserDto.Password == createUserDto.ConfirmPassword)
+			{
+
+				var appUser =  _mapper.Map<AppUser>(createUserDto);
+				appUser.UserName = createUserDto.EMail;
+				appUser.ImageID = Guid.Parse("01673030-C382-45F8-84DC-A095BF6A7532");
+				var result = await userManager.CreateAsync(appUser, createUserDto.Password);
+				if (result.Succeeded)
+				{
+					await userManager.AddToRoleAsync(appUser, "User");
+
+					return RedirectToAction("AdminDashboard", "Dashboard");
+				}
+
+				toastNotification.AddErrorToastMessage("Register operation failed!");
+				ModelState.AddModelError("", "E-mail or password is not correct.");
+			}
+			return View();
+		}
+
+		[Authorize]
+		[HttpGet]
+		public async Task<IActionResult> Logout()
+		{
+			await signInManager.SignOutAsync();
+			return RedirectToAction("Login", "Auth");
+		}
+		[Authorize]
+		[HttpGet]
+		public async Task<IActionResult> AccessDenied()
+		{
+			return View();
+		}
+		[AllowAnonymous]
+		[HttpGet]
+		public async Task<IActionResult> Error404()
+		{
+			return View();
+		}
+
+	}
+}
---
+++ MkRestaurant/MVCHamburgerciOtomasyonu/ResultMessages/Messages.cs
@@ -0,0 +1,88 @@
+﻿namespace MVCHamburgerciOtomasyonu.Web.ResultMessages
+{
+    
+    public static class Messages
+	{
+		public static class Menu
+		{
+			public static string Add(string menuName)
+			{
+				return $"{menuName} The menu titled has been added successfully.";
+			}
+			public static string Update(string menuName)
+			{
+				return $"{menuName} The menu titled has been updated successfully.";
+			}
+			public static string Delete(string menuName)
+			{
+				return $"{menuName} The menu titled has been deleted successfully.";
+			}
+			public static string UndoDelete(string menuName)
+			{
+				return $"{menuName} The menu titled has been undedeleted successfully.";
+			}
+		}
+        public static class Message
+        {
+            public static string Send(string messageName)
+            {
+                return $"{messageName} The message titled has been added successfully.";
+            }
+            
+        }
+        public static class User
+        {
+            public static string Add(string userName)
+            {
+                return $"{userName} The user with the email address has been added successfully.";
+            }
+            public static string Update(string userName)
+            {
+                return $"{userName} The user with the email address has been updated successfully..";
+            }
+            public static string Delete(string userName)
+            {
+                return $"{userName} The user with the email address has been deleted successfully.";
+            }
+        }
+        public static class MenuSize
+        {
+            public static string Add(string menuSizeName)
+            {
+                return $"{menuSizeName} The titled menu dimension has been added successfully.";
+            }
+            public static string Update(string menuSizeName)
+            {
+                return $"{menuSizeName} The titled menu dimension has been updated successfully.";
+            }
+            public static string Delete(string menuSizeName)
+            {
+                return $"{menuSizeName} The titled menu dimension has been deleted successfully.";
+            }
+            public static string UndoDelete(string menuSizeName)
+            {
+                return $"{menuSizeName} The titled menu dimension has been undodeleted successfully.";
+            }
+        }
+        public static class Extra
+        {
+            public static string Add(string extraName)
+            {
+                return $"{extraName} The extra titled has been added successfully.";
+            }
+            public static string Update(string extraName)
+            {
+                return $"{extraName} The extra titled has been updated successfully.";
+            }
+            public static string Delete(string extraName)
+            {
+                return $"{extraName} The extra titled has been deleted successfully.";
+            }
+            public static string UndoDelete(string extraName)
+            {
+                return $"{extraName} The extra titled has been undodeleted successfully.";
+            }
+        }
+    }
+
+}
